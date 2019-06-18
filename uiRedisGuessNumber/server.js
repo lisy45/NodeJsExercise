95e752f3c146c0c214aae49623aeb4e63e290946
lisy45 <1155107965@link.cuhk.edu.hk>
2019-06-18 14:49:15 +0800

    Fix problem of using static files by express.static

--- uiRedisGuessNumber/server.js
+++ uiRedisGuessNumber/server.js
@@ -4,26 +4,10 @@ var app = express();
 var redis = require('redis')
 var client = redis.createClient(6379,'127.0.0.1');
 
-var fs = require('fs');
-
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.get('/function.js', function (req, res) {
- fs.readFile('function.js', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
-});
-
-app.get('/guessNumber.html', function (req, res) {
- fs.readFile('guessNumber.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
-});
+app.use(express.static('./'))
 
 app.post('/start', function (req, res) {
 	var rand = Math.round(Math.random() * 100)
