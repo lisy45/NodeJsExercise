51f7ed2e3f92b236a2db5da456aaf43e2cd42a95
lisy45 <1155107965@link.cuhk.edu.hk>
2019-06-18 15:19:21 +0800

    Fix code style with eslint

--- uiRedisGuessNumber/function.js
+++ uiRedisGuessNumber/function.js
@@ -1,31 +1,26 @@
 function submit() {
-	var n = Number(document.getElementById("number").value)
+	const n = Number(document.getElementById('number').value)
 	if (!isNaN(n)) {
 		$.ajax({
-			type:'post',
-			url:'http://localhost:3000/' + n,
-			async:false,
-			success:function(msg) {
-				document.getElementById("result").innerHTML = msg
+			type: 'post',
+			url: 'http://localhost:3000/${n}',
+			async: false,
+			success(msg) {
+				document.getElementById('result').innerHTML = msg
 			},
-			error:function(XMLHttpRequest, textStatus, errorThrown) {
-				alert(textStatus)
-			}
-		})	
-	}
-	else 
-		alert("Please enter a number!")
+		})
+	} else { alert('Please enter a number!') }
 }
 function restart() {
 	$.ajax({
-		type:'post',
-		url:'http://localhost:3000/start',
-		async:false,
-		success:function(msg) {
+		type: 'post',
+		url: 'http://localhost:3000/start',
+		async: false,
+		success(msg) {
 			alert(msg)
-		}
+		},
 	})
 }
 function empty(obj) {
-	obj.value=""
+	obj.value = ''
 }
--- uiRedisGuessNumber/server.js
+++ uiRedisGuessNumber/server.js
@@ -1,34 +1,30 @@
-var express = require('express');
-var app = express();
+const express = require('express')
+const redis = require('redis')
+const bodyParser = require('body-parser')
 
-var redis = require('redis')
-var client = redis.createClient(6379,'127.0.0.1');
+const app = express()
+const client = redis.createClient(6379, '127.0.0.1')
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(express.static('./'))
 
-app.post('/start', function (req, res) {
-	var rand = Math.round(Math.random() * 100)
-	client.set('R',rand)
+app.post('/start', (req, res) => {
+	const rand = Math.round(Math.random() * 100)
+	client.set('R', rand)
 	res.send('OK')
-});
+})
 
-app.post('/:number', function(req,res) {
-	var number = req.params['number']
-	client.get('R',function(err, R) {
+app.post('/:number', (req, res) => {
+	const { number } = req.params
+	client.get('R', (err, R) => {
 		console.log(R)
-		if (err)
-			res.send(err)
-		else if (Number(number) < Number(R))
-			res.send('smaller')
-		else if (Number(number) > Number(R))
-			res.send('bigger')
-		else
-			res.send('equal')
-	})		
-});
+		if (err) res.send(err)
+		else if (Number(number) < Number(R)) res.send('smaller')
+		else if (Number(number) > Number(R)) res.send('bigger')
+		else { res.send('equal') }
+	})
+})
 // listen to port 3000
-app.listen(3000);
+app.listen(3000)
 console.log('Server running at port 3000.')
