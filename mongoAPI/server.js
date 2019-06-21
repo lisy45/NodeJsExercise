094f713ce9b00cd812d892fa5472bb9a3d713193
lisy45 <1155107965@link.cuhk.edu.hk>
2019-06-21 15:20:45 +0800

    Convert callback style to async style

--- mongoAPI/server.js
+++ mongoAPI/server.js
@@ -31,111 +31,127 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(session({
 	cookieName: 'session',
-	secret: 'random_string_goes_here',
+	secret: 'random_string_g oes_here',
+	duration: 30 * 60 * 1000,
 }))
 
-app.post('/register', (req, res) => {
+async function checkUser(req, res) {
+	const count = await User.countDocuments({}).exec()
+	if (count === 0) res.send('success')
+	else res.send('fail')
+}
+
+app.post('/checkUser', (req, res) => {
+	checkUser(req, res)
+})
+
+async function checkNumber(req, res) {
+	const count = await number.countDocuments({}).exec()
+	if (count === 0) res.send('success')
+	else res.send('fail')
+}
+
+app.post('/checkNumber', (req, res) => {
+	checkNumber(req, res)
+})
+
+async function register(req, res) {
 	const { name } = req.body
 	const { password } = req.body
-	User.findOne({ name })
-		.exec((err, existed) => {
-			if (err)	res.send(err)
-			if (existed)	res.send('repeated username')
-			else {
-				const salt = Math.random().toString().slice(2, 5)
-				const saltpwd = name.toString() + salt.toString() + password.toString()
-				const md5 = crypto.createHash('md5')
-				const cryptpwd = md5.update(saltpwd).digest('hex')
-				const newUser = new User({
-					name,
-					salt,
-					password: cryptpwd,
-				})
-				newUser.save((err1) => {
-					if (err1) res.send(err1)
-					else res.send('success')
-				})
-			}
+	const existed = await User.findOne({ name }).exec()
+	if (existed)	res.send('repeated username')
+	else {
+		const salt = Math.random().toString().slice(2, 5)
+		const saltpwd = name.toString() + salt.toString() + password.toString()
+		const md5 = crypto.createHash('md5')
+		const cryptpwd = md5.update(saltpwd).digest('hex')
+		const newUser = new User({
+			name,
+			salt,
+			password: cryptpwd,
 		})
+		await newUser.save()
+		res.send('success')
+	}
+}
+
+app.post('/register', (req, res) => {
+	register(req, res)
 })
+
+async function deleteUser(req, res) {
+	const name = req.params.username
+	const removed = await User.findOne({ name }).exec()
+	if (removed != null) {
+		await number.findOneAndDelete({ userid: removed._id }).exec()
+		await User.deleteOne({ name }).exec()
+		res.send('The user is successfully deleted.')
+	} else res.send('Username does not exist!')
+}
 
 app.delete('/deleteuser/:username', (req, res) => {
-	const name = req.params.username
-	User.findOne({ name }, (err, removed) => {
-		if (err) res.send(err)
-		else if (removed != null) {
-			User.deleteOne({ name }, (err1) => {
-				if (err1) res.send(err1)
-				else { res.send('The user is successfully deleted.') }
-			})
-		} else res.send('Username does not exist!')
-	})
+	deleteUser(req, res)
 })
 
-app.post('/login', (req, res) => {
+async function login(req, res) {
 	const { name } = req.body
 	const { password } = req.body
-	User.findOne({ name })
-		.exec((err, existed) => {
-			if (err)	res.send(err)
-			if (!existed)	res.send('Username does not exist!')
-			else {
-				const { salt } = existed
-				const saltpwd = name.toString() + salt.toString() + password.toString()
-				const md5 = crypto.createHash('md5')
-				const cryptpwd = md5.update(saltpwd).digest('hex')
-				if (cryptpwd != existed.password) res.send('Wrong password')
-				else {
-					number.findOne({ userid: existed._id })
-						.exec((err1, num) => {
-							if (err1)	res.send(err1)
-							else if (!num) {
-								const newNumber = new number({
-									userid: existed._id,
-								})
-								newNumber.save()
-							}
-							req.session.userid = existed._id
-							res.send(`Hello ${name}`)
-						})
-				}
+	const existed = await User.findOne({ name }).exec()
+	if (!existed)	res.send('Username does not exist!')
+	else {
+		const { salt } = existed
+		const saltpwd = name.toString() + salt.toString() + password.toString()
+		const md5 = crypto.createHash('md5')
+		const cryptpwd = md5.update(saltpwd).digest('hex')
+		if (cryptpwd !== existed.password) res.send('Wrong password')
+		else {
+			const num = await number.findOne({ userid: existed._id }).exec()
+			if (!num) {
+				const newNumber = new number({
+					userid: existed._id,
+				})
+				await newNumber.save()
 			}
-		})
+			req.session.userid = existed._id
+			res.send(`Hello ${name}`)
+		}
+	}
+}
+
+app.post('/login', (req, res) => {
+	login(req, res)
 })
 
-app.post('/start', (req, res) => {
+async function start(req, res) {
 	const sessUserid = req.session.userid
 	if (!sessUserid) res.send('Please login')
 	else {
 		const randnum = Math.round(Math.random() * 100)
-		number.findOne({ userid: sessUserid })
-			.exec((err, existed) => {
-				if (err)	res.send(err)
-				else {
-					existed.num = randnum
-					existed.save()
-					res.send('success')
-				}
-			})
+		const existed = await number.findOne({ userid: sessUserid }).exec()
+		existed.num = randnum
+		await existed.save()
+		res.send('success')
 	}
+}
+
+app.post('/start', (req, res) => {
+	start(req, res)
 })
 
-app.post('/:number', (req, res) => {
+async function check(req, res) {
 	const sessUserid = req.session.userid
 	if (!sessUserid) res.send('Please login')
 	else {
 		const guess = req.params.number
-		number.findOne({ userid: sessUserid })
-			.exec((err, existed) => {
-				if (err)	res.send(err)
-				else {
-					const answer = existed.num
-					if (Number(guess) < Number(answer)) res.send('smaller')
-					else if (Number(guess) > Number(answer)) res.send('bigger')
-					else res.send('equal')
-				}
-			})
+		const existed = await number.findOne({ userid: sessUserid }).exec()
+		const answer = existed.num
+		if (Number(guess) < Number(answer)) res.send('smaller')
+		else if (Number(guess) > Number(answer)) res.send('bigger')
+		else res.send('equal')
 	}
+}
+app.post('/:number', (req, res) => {
+	check(req, res)
 })
 
 // listen to port 3000
