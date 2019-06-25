8a6595b25ecbc756fd7abbee47e47ac58c3c86d7
lisy45 <1155107965@link.cuhk.edu.hk>
2019-06-25 11:03:35 +0800

    testAsync.js: Simplify codes and Return found value back to the top

--- asyncUiRedisGuessNumber/testAsync.js
+++ asyncUiRedisGuessNumber/testAsync.js
@@ -1,18 +1,12 @@
 const rp = require('request-promise')
 
-async function postAsync(guess) {
-	const res = await rp.post(`http://127.0.0.1:3000/${guess}`)
-	console.log(`Guess:${guess} /:number returns ${res}`)
-	if (res === 'equal') return ('Auto play completes')
-	return res
-}
-
 async function guessAsync(low, up) {
 	const guess = Math.round((low + up) / 2)
-	const res = await postAsync(guess)
-	if (res === 'bigger') guessAsync(low, guess)
-	else if (res === 'smaller') guessAsync(guess, up)
-	else console.log(res)
+	const res = await rp.post(`http://127.0.0.1:3000/${guess}`)
+	console.log(`Guess:${guess} /:number returns ${res}`)
+	if (res === 'bigger') return guessAsync(low, guess)
+	if (res === 'smaller') return guessAsync(guess, up)
+	return guess
 }
 
 async function playByAsync() {
@@ -20,7 +14,8 @@ async function playByAsync() {
 	const res = await rp.post('http://127.0.0.1:3000/start')
 	if (res === 'OK') {
 		console.log('/start returns OK')
-		guessAsync(0, 1000000)
+		const result = await guessAsync(0, 1000000)
+		console.log(`Find answer ${result}`)
 	} else console.log('/start fails')
 }
 playByAsync()
