b29554f59f33329c953c98988a5a78c536dfef15
lisy45 <1155107965@link.cuhk.edu.hk>
2019-06-25 15:39:17 +0800

    testPromise.js: Modified

--- asyncUiRedisGuessNumber/testPromise.js
+++ asyncUiRedisGuessNumber/testPromise.js
@@ -1,19 +1,16 @@
 const rp = require('request-promise')
 
 function guessPromise(low, up) {
-	return new Promise((resolve) => {
-		function recur(l, u) {
-			const guess = Math.round((l + u) / 2)
-			rp.post(`http://127.0.0.1:3000/${guess}`)
-				.then((res) => {
-					console.log(`Guess:${guess} /:number returns ${res}`)
-					if (res === 'bigger') recur(l, guess)
-					else if (res === 'smaller') recur(guess, u)
-					else resolve(guess)
-				})
-		}
-		recur(low, up)
-	})
+	const guess = Math.round((low + up) / 2)
+	return rp.post(`http://127.0.0.1:3000/${guess}`)
+		.then((res) => {
+			console.log(`Guess:${guess} /:number returns ${res}`)
+			if (res === 'bigger') return guessPromise(low, guess)
+			if (res === 'smaller') return guessPromise(guess, up)
+			return new Promise((resolve) => {
+				resolve(guess)
+			})
+		})
 }
 
 function playByPromise() {
